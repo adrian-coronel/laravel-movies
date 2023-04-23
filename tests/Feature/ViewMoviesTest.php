b32448459c635fabcec18954caee6b3976e5df13
Adrian Coronel <adrianonel78@gmail.com>
2023-04-22 20:41:06 -0500

    Test para el componente search-dropdown

--- tests/Feature/ViewMoviesTest.php
+++ tests/Feature/ViewMoviesTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 
 use Illuminate\Support\Facades\Http;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 
@@ -54,6 +55,20 @@ class ViewMoviesTest extends TestCase
     $response->assertSee('Jeanne McCarthy');
     $response->assertSee('Casting Director');
     $response->assertSee('Dwayne Johnson');
+  }
+
+  public function test_the_search_dropdown_works_correctly()
+  {
+    Http::fake([
+      'https://api.themoviedb.org/3/search/movie?query=jumanji' => $this->fakeSearchMovies(),
+    ]);
+
+    # Probamos el componente search-dropdown
+    Livewire::test('search-dropdown') # Se crea una instancia del componente, para poder usar los siguientes metodos
+      ->assertDontSee('jumanji')# verifica que el componenete no muestre el termino 'jumanji'
+      ->set('search','jumanji')# Simula una busqueda, 'search' => variable de busqueda y 'jumanji' es el valor que le damos
+      ->assertSee('Jumanji');# verifica que el componente muestre el término "Jumanji" después de realizar la búsqueda.
+
   }
 
   private function fakePopularMovies(){
@@ -201,83 +216,112 @@ class ViewMoviesTest extends TestCase
     ,200);
   }
 
-  public function fakeSingleMovie()
-    {
+  private function fakeSingleMovie()
+  {
+    return Http::response([
+      "adult" => false,
+      "backdrop_path" => "/hreiLoPysWG79TsyQgMzFKaOTF5.jpg",
+      "genres" => [
+          ["id" => 28, "name" => "Action"],
+          ["id" => 12, "name" => "Adventure"],
+          ["id" => 35, "name" => "Comedy"],
+          ["id" => 14, "name" => "Fantasy"],
+      ],
+      "homepage" => "http://jumanjimovie.com",
+      "id" => 12345,
+      "overview" => "As the gang return to Jumanji to rescue one of their own, they discover that nothing is as they expect. The players will have to brave parts unknown and unexplored.",
+      "poster_path" => "/bB42KDdfWkOvmzmYkmK58ZlCa9P.jpg",
+      "release_date" => "2019-12-04",
+      "runtime" => 123,
+      "title" => "Fake Jumanji: The Next Level",
+      "vote_average" => 6.8,
+      "credits" => [
+          "cast" => [
+              [
+                  "cast_id" => 2,
+                  "character" => "Dr. Smolder Bravestone",
+                  "credit_id" => "5aac3960c3a36846ea005147",
+                  "gender" => 2,
+                  "id" => 18918,
+                  "name" => "Dwayne Johnson",
+                  "order" => 0,
+                  "profile_path" => "/kuqFzlYMc2IrsOyPznMd1FroeGq.jpg",
+              ]
+          ],
+          "crew" => [
+              [
+                  "credit_id" => "5d51d4ff18b75100174608d8",
+                  "department" => "Production",
+                  "gender" => 1,
+                  "id" => 546,
+                  "job" => "Casting Director",
+                  "name" => "Jeanne McCarthy",
+                  "profile_path" => null,
+              ]
+          ]
+      ],
+      "videos" => [
+          "results" => [
+              [
+                  "id" => "5d1a1a9b30aa3163c6c5fe57",
+                  "iso_639_1" => "en",
+                  "iso_3166_1" => "US",
+                  "key" => "rBxcF-r9Ibs",
+                  "name" => "JUMANJI: THE NEXT LEVEL - Official Trailer (HD)",
+                  "site" => "YouTube",
+                  "size" => 1080,
+                  "type" => "Trailer",
+              ]
+          ]
+      ],
+      "images" => [
+          "backdrops" => [
+              [
+                  "aspect_ratio" => 1.7777777777778,
+                  "file_path" => "/hreiLoPysWG79TsyQgMzFKaOTF5.jpg",
+                  "height" => 2160,
+                  "iso_639_1" => null,
+                  "vote_average" => 5.388,
+                  "vote_count" => 4,
+                  "width" => 3840,
+              ]
+          ],
+          "posters" => [
+              [
+
+              ]
+          ]
+      ]
+    ], 200);
+  }
+
+  private function fakeSearchMovies()
+  {
       return Http::response([
-        "adult" => false,
-        "backdrop_path" => "/hreiLoPysWG79TsyQgMzFKaOTF5.jpg",
-        "genres" => [
-            ["id" => 28, "name" => "Action"],
-            ["id" => 12, "name" => "Adventure"],
-            ["id" => 35, "name" => "Comedy"],
-            ["id" => 14, "name" => "Fantasy"],
-        ],
-        "homepage" => "http://jumanjimovie.com",
-        "id" => 12345,
-        "overview" => "As the gang return to Jumanji to rescue one of their own, they discover that nothing is as they expect. The players will have to brave parts unknown and unexplored.",
-        "poster_path" => "/bB42KDdfWkOvmzmYkmK58ZlCa9P.jpg",
-        "release_date" => "2019-12-04",
-        "runtime" => 123,
-        "title" => "Fake Jumanji: The Next Level",
-        "vote_average" => 6.8,
-        "credits" => [
-            "cast" => [
-                [
-                    "cast_id" => 2,
-                    "character" => "Dr. Smolder Bravestone",
-                    "credit_id" => "5aac3960c3a36846ea005147",
-                    "gender" => 2,
-                    "id" => 18918,
-                    "name" => "Dwayne Johnson",
-                    "order" => 0,
-                    "profile_path" => "/kuqFzlYMc2IrsOyPznMd1FroeGq.jpg",
-                ]
-            ],
-            "crew" => [
-                [
-                    "credit_id" => "5d51d4ff18b75100174608d8",
-                    "department" => "Production",
-                    "gender" => 1,
-                    "id" => 546,
-                    "job" => "Casting Director",
-                    "name" => "Jeanne McCarthy",
-                    "profile_path" => null,
-                ]
-            ]
-        ],
-        "videos" => [
-            "results" => [
-                [
-                    "id" => "5d1a1a9b30aa3163c6c5fe57",
-                    "iso_639_1" => "en",
-                    "iso_3166_1" => "US",
-                    "key" => "rBxcF-r9Ibs",
-                    "name" => "JUMANJI: THE NEXT LEVEL - Official Trailer (HD)",
-                    "site" => "YouTube",
-                    "size" => 1080,
-                    "type" => "Trailer",
-                ]
-            ]
-        ],
-        "images" => [
-            "backdrops" => [
-                [
-                    "aspect_ratio" => 1.7777777777778,
-                    "file_path" => "/hreiLoPysWG79TsyQgMzFKaOTF5.jpg",
-                    "height" => 2160,
-                    "iso_639_1" => null,
-                    "vote_average" => 5.388,
-                    "vote_count" => 4,
-                    "width" => 3840,
-                ]
-            ],
-            "posters" => [
-                [
-
-                ]
-            ]
-        ]
-      ], 200);
-    }
-
+              'results' => [
+                  [
+                      "popularity" => 406.677,
+                      "vote_count" => 2607,
+                      "video" => false,
+                      "poster_path" => "/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg",
+                      "id" => 419704,
+                      "adult" => false,
+                      "backdrop_path" => "/5BwqwxMEjeFtdknRV792Svo0K1v.jpg",
+                      "original_language" => "en",
+                      "original_title" => "Jumanji",
+                      "genre_ids" => [
+                          12,
+                          18,
+                          9648,
+                          878,
+                          53,
+                      ],
+                      "title" => "Jumanji",
+                      "vote_average" => 6,
+                      "overview" => "Jumanji description. The near future, a time when both hope and hardships drive humanity to look to the stars and beyond. While a mysterious phenomenon menaces to destroy life on planet earth.",
+                      "release_date" => "2019-09-17",
+                  ]
+              ]
+          ], 200);
+  }
 }
